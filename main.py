e2dc3e913f7e959ef14458431f99c96b8a94242d
Ian Howell <ian.howell0@gmail.com>
2017-05-26 11:31:34 -0500

    Add function for constructing the email

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 from comic import get_newest_xkcd
-from weather import get_current_weather
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from weather import format_weather, get_current_weather
 
 def main():
     username = input("Username: ")
@@ -10,6 +13,28 @@ def main():
 
     xkcd_info = get_newest_xkcd()
     weather_info = get_current_weather(api_key, city_id)
+
+    message = build_message(username, xkcd_info, weather_info)
+    print()
+    print(message)
+
+def build_message(email_addr, xkcd_info, weather_info):
+    # Load the image to be sent
+    img_file = open(xkcd_info['img'], 'rb').read()
+
+    # Set up the body of the email (text)
+    formatted_weather = format_weather(weather_info)
+    email_body = "\n{}\n".format(xkcd_info['alt'])
+    email_body += "\n{}".format(formatted_weather)
+
+    # Set up the email
+    message = MIMEMultipart()
+    message['Subject'] = xkcd_info['title']
+    message['To'] = email_addr
+    message.attach(MIMEImage(img_file))
+    message.attach(MIMEText(email_body))
+
+    return message
 
 if __name__ == "__main__":
    main()
