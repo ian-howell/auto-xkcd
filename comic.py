b07ded0bbabb9f56ebb50d39653d92b12024d133
Ian Howell <ian.howell0@gmail.com>
2017-05-26 10:39:13 -0500

    First commit in overhaul: fix comic downloading

---
+++ comic.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import json
+import requests
+
+def get_comic_info():
+    api_string = "http://xkcd.com/info.0.json"
+    res = requests.get(api_string)
+    xkcd_info = json.loads(res.text)
+    minimal_info = {
+            'title': xkcd_info['title'],
+            'alt': xkcd_info['alt'],
+            'img': xkcd_info['img'],
+            }
+    return minimal_info
+
+def download_comic(img):
+    res = requests.get(img['img'])
+    img_filename = "/tmp/" + img['title'] + '.png'
+    with open(img_filename, 'wb') as img_file:
+        for chunk in res.iter_content(100000):
+            img_file.write(chunk)
+    return img_filename
+
+def get_newest_xkcd():
+    xkcd_info = get_comic_info()
+    filename = download_comic(xkcd_info)
+
+    # Modify the img's path to be the local version
+    xkcd_info['img'] = filename
+    return xkcd_info
---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from comic import get_newest_xkcd
+
+def main():
+    print(get_newest_xkcd())
+
+
+if __name__ == "__main__":
+   main()
