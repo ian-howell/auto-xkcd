c62a58f485ba8222d1c7d2b58003557bd62782fc
Ian Howell <ian.howell0@gmail.com>
2018-09-08 10:44:53 -0500

    Switch from reading from stdin to using configparser

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
 from comic import get_newest_xkcd
+from configparser import ConfigParser
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from sender import send_email
+import sys
 from weather import format_weather, get_current_weather
 
 
-def main():
-    email_addr = input("Email address: ")
-    password = input("Password: ")
-    api_key = input("API key: ")
-    city_id = input("City ID: ")
+def main(args):
+    config = ConfigParser()
+    config.read(args[1])
+
+    email = config['EMAIL']
+    weather = config['WEATHER']
 
     try:
         message_content = {}
         message_content['xkcd_info'] = get_newest_xkcd()
-        message_content['weather_info'] = get_current_weather(api_key, city_id)
+        message_content['weather_info'] = get_current_weather(
+            weather['api_key'], weather['city_id'])
 
-        message = build_message(email_addr, message_content)
+        message = build_message(email['address'], message_content)
 
-        send_email(email_addr, password, message)
+        send_email(email['address'], email['password'], message)
     except Exception as e:
-        error_message = build_error(email_addr, password, e)
-        send_email(email_addr, password, error_message)
+        error_message = build_error(email['address'], email['password'], e)
+        send_email(email['address'], email['password'], error_message)
 
 
 def build_message(email_addr, message_content):
@@ -68,4 +72,4 @@ def build_error(email_addr, password, e):
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
