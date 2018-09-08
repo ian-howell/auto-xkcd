ed47d9c1c814a9ae353aa4e6f3e6bff714b2db22
Ian Howell <ian.howell0@gmail.com>
2018-09-08 11:59:09 -0500

    Clean up auxilary files

--- weather.py
+++ weather.py
@@ -3,31 +3,10 @@ import json
 import requests
 
 
-def get_weather_info(api_string):
-    res = requests.get(api_string)
-    weather_info = json.loads(res.text)
-    minimal_info = {
-            'city': weather_info['name'],
-            'temp': kelv_to_fahr(weather_info['main']['temp']),
-            }
-    return minimal_info
-
-
 def get_current_weather(key, city):
-    api_string = build_url(key, city)
-    weather_info = get_weather_info(api_string)
+    api_string = _build_url(key, city)
+    weather_info = _get_weather_info(api_string)
     return weather_info
-
-
-def kelv_to_fahr(temp):
-    return (temp - 273.15) * 1.8 + 32
-
-
-def build_url(key, city):
-    city_id = "id={}".format(city)
-    app_id = "APPID={}".format(key)
-    base_url = "http://api.openweathermap.org/data/2.5/weather"
-    return "{}?{}&{}".format(base_url, city_id, app_id)
 
 
 def format_weather(weather_info):
@@ -35,3 +14,24 @@ def format_weather(weather_info):
     formatted = "Current temperature in {}".format(weather_info['city'])
     formatted += ": {:.1f} {}F\n".format(weather_info['temp'], degree_char)
     return formatted
+
+
+def _get_weather_info(api_string):
+    res = requests.get(api_string)
+    weather_info = json.loads(res.text)
+    minimal_info = {
+            'city': weather_info['name'],
+            'temp': _kelv_to_fahr(weather_info['main']['temp']),
+            }
+    return minimal_info
+
+
+def _kelv_to_fahr(temp):
+    return (temp - 273.15) * 1.8 + 32
+
+
+def _build_url(key, city):
+    city_id = "id={}".format(city)
+    app_id = "APPID={}".format(key)
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    return "{}?{}&{}".format(base_url, city_id, app_id)
