5ff7cf18877ec04fc43b000afa4b6df8a86057da
Ian Howell <ian.howell0@gmail.com>
2018-09-08 11:57:15 -0500

    Fix error handling

--- main.py
+++ main.py
@@ -23,16 +23,19 @@ def main():
 
     message_content = {}
     if sections['comic'] == 'on':
-        message_content['xkcd_info'] = get_newest_xkcd()
+        try:
+            message_content['xkcd_info'] = get_newest_xkcd()
+        except Exception as e:
+            message_content['xkcd_info'] = e
     if sections['weather'] == 'on':
-        message_content['weather_info'] = get_current_weather(
-            weather['api_key'], weather['city_id'])
+        try:
+            message_content['weather_info'] = get_current_weather(
+                weather['api_key'], weather['city_id'])
+        except Exception as e:
+            message_content['weather_info'] = e
 
     message = build_message(email['address'], message_content)
     send_email(email, message)
-
-    # error_message = build_error(email['address'], email['password'], e)
-    # send_email(email['address'], email['password'], error_message)
 
 
 def build_message(email_addr, message_content):
@@ -42,41 +45,34 @@ def build_message(email_addr, message_content):
 
     email_body = ''
     if 'xkcd_info' in message_content.keys():
-        xkcd_info = message_content['xkcd_info']
-        message.replace_header('Subject', xkcd_info['title'])
-        email_body += "\n{}\n".format(xkcd_info['alt'])
+        if isinstance(message_content['xkcd_info'], Exception):
+            email_body += ("Something went wrong when trying to download "
+                           "the latest xkcd. Here is the stacktrace:\n")
+            email_body += str(message_content['xkcd_info'])
+        else:
+            xkcd_info = message_content['xkcd_info']
+            message.replace_header('Subject', xkcd_info['title'])
+            email_body += "\n{}\n".format(xkcd_info['alt'])
 
-        # Load the image to be sent
-        img_file = open(xkcd_info['img'], 'rb').read()
-        message.attach(MIMEImage(img_file))
+            # Load the image to be sent
+            img_file = open(xkcd_info['img'], 'rb').read()
+            message.attach(MIMEImage(img_file))
 
     if 'weather_info' in message_content.keys():
-        weather_info = message_content['weather_info']
+        if isinstance(message_content['weather_info'], Exception):
+            email_body += ("Something went wrong when trying to download "
+                           "weather info. Here is the stacktrace:\n")
+            email_body += str(message_content['weather_info'])
+        else:
+            weather_info = message_content['weather_info']
 
-        # Set up the body of the email (text)
-        formatted_weather = format_weather(weather_info)
-        email_body += "\n{}".format(formatted_weather)
+            # Set up the body of the email (text)
+            formatted_weather = format_weather(weather_info)
+            email_body += "\n{}".format(formatted_weather)
 
     message['To'] = email_addr
     message['From'] = email_addr
     message.attach(MIMEText(email_body))
-
-    return message
-
-
-def build_error(email_addr, password, e):
-    raw_except = str(type(e))
-    idx_1 = raw_except.find('\'') + 1
-    idx_2 = raw_except.find('\'', idx_1)
-    exception_name = raw_except[idx_1:idx_2]
-
-    error_message = "A {} was caught\n".format(exception_name)
-    error_message += "Exception message: {}".format(e)
-
-    message = MIMEText(error_message)
-    message['Subject'] = "Subject: Caught a {}\n".format(exception_name)
-    message['To'] = email_addr
-    message['From'] = email_addr
 
     return message
 
